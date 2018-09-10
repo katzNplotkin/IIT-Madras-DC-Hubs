e97e7632283009f2bbb4778e55f9f2b613722c87
cibinjoseph <cibinjoseph92@gmail.com>
2018-09-11 02:01:03 +0530

    Create README file

--- check_hub_stat.py
+++ check_hub_stat.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-# Checks status of IIT Madras Hubs
+# Checks status of IIT Madras DC Hubs
 
 import nmap
+import datetime
 
 # Get hub ip and corresponding ports from hublist.txt to variable hublist
 with open('hublist.txt','r') as hublistfile:
@@ -10,6 +11,7 @@ with open('hublist.txt','r') as hublistfile:
 # Check status of hubs using ping to port
 nm=nmap.PortScanner()    # Init PortScanner object
 with open('hubstat.txt','w') as hubstatfile:
+    hubstatfile.write('Last Updated: {:%c}\n\n'.format(datetime.datetime.now()))
     for hub in hublist:
         if hub:    # Handle empty lines
             if hub[0] !='#':    # Handle comments
@@ -27,3 +29,8 @@ with open('hubstat.txt','w') as hubstatfile:
                 hubstatfile.write(hubName+'\t'+hub[1]+'\t'+hubmode+'\n')
 
 # Write to README file
+readmeParts=['docs/README_head.md','hubstat.txt','docs/README_tail.md']
+with open('docs/README.md','w') as readmeFile:
+    for fname in readmeParts:
+        with open(fname,'r') as infile:
+            readmeFile.write(infile.read())
