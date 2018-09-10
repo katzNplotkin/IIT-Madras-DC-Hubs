4aebb108aa1a40ca39d77cb78e6050d6ffeea88c
cibinjoseph <cibinjoseph92@gmail.com>
2018-09-11 03:24:14 +0530

    Add Update commit script

---
+++ update_commit.sh
@@ -0,0 +1,2 @@
+git add hubstat.txt docs/README.md
+git commit -m 'Hub status auto-update'
--- update_hub_status.sh
+++ update_hub_status.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Updates hub status by calling check_hub_stat.py
 # This script may be scheduled as a cronjob for auto-updation of website or README.md
+
+cd ~/WorkInProgress/IIT-Madras-DC-Hubs
+git pull 
+python check_hub_status.py
+bash update_commit.sh
+git push
